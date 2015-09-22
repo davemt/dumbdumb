6961042a963d724bda050960d0f4b91c8fc52675
David Thompson <dthompson@compete.com>
2015-09-21 21:52:14 -0400

    add 411 phone directory handler

--- dumbdumb/dumbdumb.go
+++ dumbdumb/dumbdumb.go
@@ -5,19 +5,24 @@ import (
 	"dumbdumb/handler"
 	"dumbdumb/listener"
 	"os"
+	"strings"
 )
 
 func main() {
 	server := &dumbdumb.Server{}
 
 	// Whitespace-separated list of email address domains
-	domainWhitelist = os.Getenv("DUMBDUMB_SMTP_SENDER_DOMAIN_WHITELIST")
+	domainWhitelist := os.Getenv("DUMBDUMB_SMTP_SENDER_DOMAIN_WHITELIST")
 	server.AddListener(listener.SMTPListener{
 		DomainWhitelist: strings.Fields(domainWhitelist),
 	})
 
 	server.AddHandler("[Ww]eather.*", handler.WeatherHandler{
 		WuApiKey: os.Getenv("DUMBDUMB_WEATHERUNDERGROUND_API_KEY"),
+	})
+
+	server.AddHandler("411", handler.PhoneDirectoryHandler{
+		GoogleAPIKey: os.Getenv("DUMBDUMB_GOOGLE_API_KEY"),
 	})
 
 	server.ListenAndServe()
---
+++ handler/phonedir.go
@@ -0,0 +1,71 @@
+package handler
+
+import (
+	"dumbdumb"
+	"fmt"
+	"github.com/jmoiron/jsonq"
+	"log"
+	"napping"
+	"strings"
+)
+
+type PhoneDirectoryHandler struct {
+	GoogleAPIKey string
+}
+
+func (h PhoneDirectoryHandler) HandleRequest(request dumbdumb.Request) error {
+	log.Printf("PhoteDirectoryHandler got request: %v", request.GetPayload())
+	parts := strings.SplitAfterN(request.GetPayload(), " ", 2)
+	_, placeQuery := parts[0], parts[1]
+
+	params := napping.Params{
+		"key":   h.GoogleAPIKey,
+		"query": placeQuery,
+	}
+
+	var data map[string]interface{}
+
+	// make request to get place id based on text query
+	resp, err := napping.Get(
+		"https://maps.googleapis.com/maps/api/place/textsearch/json",
+		&params, &data, nil)
+	if resp.Status() != 200 {
+		return err
+	}
+
+	jq := jsonq.NewQuery(data)
+
+	// assume the first match is the best
+	placeId, err := jq.String("results", "0", "place_id")
+
+	// use place id to query place details
+	params = napping.Params{
+		"key":     h.GoogleAPIKey,
+		"placeid": placeId,
+	}
+
+	resp, err = napping.Get(
+		"https://maps.googleapis.com/maps/api/place/details/json",
+		&params, &data, nil)
+	if resp.Status() != 200 {
+		return err
+	}
+
+	jq = jsonq.NewQuery(data)
+
+	result, err := jq.Object("result")
+	if err != nil {
+		return err
+	}
+	placeName := result["name"].(string)
+	phoneNumber := result["international_phone_number"].(string)
+	address := result["formatted_address"].(string)
+	openNow, err := jq.Bool("opening_hours", "open_now")
+	if err != nil {
+		return err
+	}
+
+	err = request.SendOutput(fmt.Sprintf("%v\nPhone: %v\nAddr: %v\nOpen now: %v",
+		placeName, phoneNumber, address, openNow))
+	return err
+}
