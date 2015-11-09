6c58afd76ed002723f22bfab671da43e09536827
David Thompson <dthompson@compete.com>
2015-11-09 08:12:13 -0500

    add source/target lang support to Translate handler

--- handler/translate.go
+++ handler/translate.go
@@ -2,29 +2,46 @@ package handler
 
 import (
 	"dumbdumb"
+	"errors"
 	"fmt"
 	"github.com/jmoiron/jsonq"
 	"log"
 	"napping"
-	"strings"
+	"regexp"
 )
 
 type TranslateHandler struct {
 	GoogleAPIKey string
 }
 
-func (h TranslateHandler) HandleRequest(request dumbdumb.Request) error {
-	log.Printf("TranslateHandler got request: %v", request.GetPayload())
-	parts := strings.SplitAfterN(request.GetPayload(), " ", 2)
-	_, query := parts[0], parts[1]
-
-	// TODO: add support for to/from languages
-	// TODO: test sending erroneous keys
-	// TODO: test 403 forbidden error handling (when setting up new Google API)
+func (h TranslateHandler) BuildAPIParamsFromRequest(request dumbdumb.Request) (napping.Params, error) {
+	// TODO store precompiled on handler or global
+	exp := regexp.MustCompile(
+		// e.g. "Translate en -> fr lobster"
+		"^[Tt]ranslate\\s(?P<source>[a-z]{2})\\s->\\s(?P<target>[a-z]{2})\\s(?P<q>.*)$")
+	match := exp.FindStringSubmatch(request.GetPayload())
+	if match == nil || len(match) != 4 {
+		return nil, errors.New("invalid request format")
+	}
 	params := napping.Params{
 		"key":    h.GoogleAPIKey,
-		"q":      query,
-		"target": "en",
+		"source": match[1],
+		"target": match[2],
+		"q":      match[3],
+	}
+	return params, nil
+}
+
+// TODO: test sending erroneous keys
+// TODO: test 403 forbidden error handling (when setting up new Google API)
+func (h TranslateHandler) HandleRequest(request dumbdumb.Request) error {
+	log.Printf("TranslateHandler got request: %v", request.GetPayload())
+
+	params, err := h.BuildAPIParamsFromRequest(request)
+	if err != nil {
+		log.Printf("TranslateHandler could not parse request '%v': %v",
+			request.GetPayload(), err)
+		return err
 	}
 
 	var data map[string]interface{}
@@ -39,16 +56,16 @@ func (h TranslateHandler) HandleRequest(request dumbdumb.Request) error {
 
 	jq := jsonq.NewQuery(data)
 
-	// assume the first match is the best
-	detectedSource, err := jq.String("data", "translations", "0", "detectedSourceLanguage")
+	// assume the first translation is the one we want
+	//sourceLang, err := jq.String("data", "translations", "0", "detectedSourceLanguage")
 	result, err := jq.String("data", "translations", "0", "translatedText")
 
 	if err != nil {
 		return err
 	}
 
-	err = request.SendOutput(fmt.Sprintf("en -> %v: %v\n",
-		detectedSource, result))
+	err = request.SendOutput(fmt.Sprintf("%s -> %v: %v\n",
+		params["source"], params["target"], result))
 
 	return err
 }
---
+++ handler/translate_test.go
@@ -0,0 +1,57 @@
+package handler
+
+import (
+	"fmt"
+	"testing"
+)
+
+type MockRequest struct {
+	Payload string
+	Sender  string
+}
+
+func (r MockRequest) GetPayload() string             { return r.Payload }
+func (r MockRequest) SendOutput(output string) error { return nil }
+
+func makeRequest(payload string) MockRequest {
+	return MockRequest{Payload: payload}
+}
+
+func makeHandler() TranslateHandler {
+	return TranslateHandler{GoogleAPIKey: "123abcd"}
+}
+
+func TestBuildAPIParamsFromRequest(t *testing.T) {
+	h := makeHandler()
+	r := makeRequest("Translate en -> fr lobster")
+
+	params, err := h.BuildAPIParamsFromRequest(r)
+
+	if err != nil {
+		t.Error(fmt.Sprintf("Failed to parse a request that was valid: %v", err))
+	}
+	if params["key"] != "123abcd" {
+		t.Error("Failed to add google API key to params")
+	}
+	if params["source"] != "en" || params["target"] != "fr" {
+		t.Error("Failed to parse source/target langs from request")
+	}
+	if params["q"] != "lobster" {
+		t.Error("Failed to parse to-translate phrase from request")
+	}
+}
+
+func TestBuildAPIParamsFromRequestInvalid(t *testing.T) {
+	h := makeHandler()
+	r := makeRequest("Translate en - lobster")
+
+	params, err := h.BuildAPIParamsFromRequest(r)
+
+	if err == nil {
+		t.Error(fmt.Sprintf("Should have returned error with invalid request"))
+	}
+
+	if params != nil {
+		t.Error(fmt.Sprintf("Should have returned nil params with invalid request"))
+	}
+}
